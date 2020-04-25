13e7f07d04720e876fee2bbf83935a8c4794a9f0
karunjoseph <karun.mathew.joseph@vw-fc.com>
2020-04-25 12:08:37 +0200

    Updates

---
+++ precache-manifest.0ea24e6d7eb43b922325b5408b030555.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d61666d91d0d4f15bef4bfea25eafcfa",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ca5ed633aa083f95d6ad",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f53f1b9670f2e60ad904",
+    "url": "/portfolio/static/js/2.4270715f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.4270715f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca5ed633aa083f95d6ad",
+    "url": "/portfolio/static/js/main.ae0bb53d.chunk.js"
+  },
+  {
+    "revision": "b5fdca0d6b846d583dec",
+    "url": "/portfolio/static/js/runtime-main.411b09f1.js"
+  }
+]);
