350a7a9fdd3ca2dd92ab610fe5567f91c0ba9127
karunjoseph <karun.mathew.joseph@vw-fc.com>
2020-04-25 12:04:31 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Link } from 'react-router-dom'; 
+import Work from './Work.js'; 
+import Process from './Process.js'; 
+import About from './About.js'; 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+  
+        <Route exact path="/" component={About} />
+        <Route path="/process" component={Process} />
+        <Route path="/work" component={Work} />
+  
+        <div className="navigation">
+          <div className="navigation-sub">
+                                          
+            <Link to="/" className="item">About</Link>
+            <Link to="/process" className="item">Process</Link>
+            <Link to="/work" className="item">Work</Link>
+  
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Process.js
@@ -0,0 +1,11 @@
+import React from "react"
+ 
+function Process(props) {
+  return (
+    <div>
+      <h1>Process</h1>
+    </div>
+  )
+}
+ 
+export default Process;
